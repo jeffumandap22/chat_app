c8a9d30b40d0bc4c986db0522074fa6a8a3b4649
Jeffrey Umandap <jeffreyumandap@Jeff-Umandap-MacBook-Pro.local>
2018-12-05 17:53:55 +0800

    web request created

--- LetsChat/LetsChat/Utilities/Constants.swift
+++ LetsChat/LetsChat/Utilities/Constants.swift
@@ -8,7 +8,20 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) ->()
+
+// url
+let BASE_URL = "https://letsgochat.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 //segue
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+//user defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
+
