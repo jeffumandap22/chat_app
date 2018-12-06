008765c89708a7de9971d48ca1189cd619520023
Jeffrey Umandap <jeffreyumandap@Jeff-Umandap-MacBook-Pro.local>
2018-12-06 12:59:46 +0800

    Logging a user

--- LetsChat/LetsChat/Utilities/Constants.swift
+++ LetsChat/LetsChat/Utilities/Constants.swift
@@ -13,6 +13,7 @@ typealias CompletionHandler = (_ Success: Bool) ->()
 // url
 let BASE_URL = "https://letschattt.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
+let URL_LOGIN = "\(BASE_URL)account/login"
 
 //segue
 let TO_LOGIN = "toLogin"
@@ -24,4 +25,9 @@ let TOKEN_KEY = "token"
 let LOGGED_IN_KEY = "loggedIn"
 let USER_EMAIL = "userEmail"
 
+//headers
+
+let HEADER = [
+    "Content-Type": "application/json; charset=utf-8"
+]
 
