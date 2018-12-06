4d47d3e693986cf4d9de66beca64c44764e4a3a5
Jeffrey Umandap <jeffreyumandap@Jeff-Umandap-MacBook-Pro.local>
2018-12-06 18:25:31 +0800

    Logged in Interface

--- LetsChat/LetsChat/Controller/ChannelVC.swift
+++ LetsChat/LetsChat/Controller/ChannelVC.swift
@@ -12,18 +12,32 @@ import UIKit
 class ChannelVC: UIViewController {
 
     @IBOutlet weak var loginButton: UIButton!
-    @IBAction func prepareForUnwind(segue: UIStoryboardSegue){
-        
-    }
+    @IBOutlet weak var userImg: CircleImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+    }
+    
+    @IBAction func prepareForUnwind(segue: UIStoryboardSegue){
+        
     }
     
     @IBAction func loginButtonPress(_ sender: Any) {
         performSegue(withIdentifier: TO_LOGIN, sender: nil)
     }
-
+    
+    @objc func userDataDidChange(_ notif: Notification) {
+        if AuthService.instance.isLoggedIn {
+            loginButton.setTitle(UserDataService.instance.name, for: .normal)
+            userImg.image = UIImage(named: UserDataService.instance.avatarName)
+            userImg.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+        } else {
+            loginButton.setTitle("Login", for: .normal)
+            userImg.image = UIImage(named: "menuProfileIcon")
+            userImg.backgroundColor = UIColor.clear
+        }
+    }
 }
--- LetsChat/LetsChat/Services/UserDataService.swift
+++ LetsChat/LetsChat/Services/UserDataService.swift
@@ -28,4 +28,35 @@ class UserDataService {
     func setAvatarName(avatarName: String) {
         self.avatarName = avatarName
     }
+    
+    func returnUIColor(components: String) -> UIColor {
+        //return this color format
+        //"avatarColor": "[0.984313725490196,0.411764705882353,0.6,1]",
+        
+        let scanner = Scanner(string: components)
+        let skipped = CharacterSet(charactersIn: "[], ")
+        let comma = CharacterSet(charactersIn: ",")
+        scanner.charactersToBeSkipped = skipped
+        
+        var r, g, b, a : NSString?
+        scanner.scanUpToCharacters(from: comma, into: &r)
+        scanner.scanUpToCharacters(from: comma, into: &g)
+        scanner.scanUpToCharacters(from: comma, into: &b)
+        scanner.scanUpToCharacters(from: comma, into: &a)
+        
+        let defaultColor = UIColor.lightGray
+        
+        guard let rUnwrapped = r else { return defaultColor }
+        guard let gUnwrapped = g else { return defaultColor }
+        guard let bUnwrapped = b else { return defaultColor }
+        guard let aUnwrapped = a else { return defaultColor }
+        
+        let rFloat = CGFloat(rUnwrapped.doubleValue)
+        let gFloat = CGFloat(gUnwrapped.doubleValue)
+        let bFloat = CGFloat(bUnwrapped.doubleValue)
+        let aFloat = CGFloat(aUnwrapped.doubleValue)
+        
+        let newUIColor = UIColor(red: rFloat, green: gFloat, blue: bFloat, alpha: aFloat)
+        return newUIColor
+    }
 }
