1a1c4e4b474d3179f00971aff0c6cf76310e763d
Jeffrey Umandap <jeffreyumandap@Jeff-Umandap-MacBook-Pro.local>
2018-12-10 18:52:21 +0800

    Fetching Messages

--- LetsChat/LetsChat/Controller/ChatVC.swift
+++ LetsChat/LetsChat/Controller/ChatVC.swift
@@ -48,13 +48,27 @@ class ChatVC: UIViewController {
     func updateWithChannel() {
         let channelName = MessageService.instance.selectedChannel?.channelTitle ?? ""
         channelNameLabel.text = "#\(channelName)"
+        getMessages()
     }
     
     func onLoginGetMessages() {
         MessageService.instance.findAllChannel { (success) in
             if success {
-                // do stuff with channels
+                if MessageService.instance.channels.count > 0 {
+                    MessageService.instance.selectedChannel = MessageService.instance.channels[0]
+                    self.updateWithChannel()
+                } else {
+                    self.channelNameLabel.text = "No channels yet!"
+                }
+                
             }
+        }
+    }
+    
+    func getMessages() {
+        guard let channelId = MessageService.instance.selectedChannel?.id else { return }
+        MessageService.instance.findAllMessagesForChannel(channelId: channelId) { (success) in
+            
         }
     }
 }
---
+++ LetsChat/LetsChat/Model/Message.swift
@@ -0,0 +1,19 @@
+//
+//  Message.swift
+//  LetsChat
+//
+//  Created by Jeffrey Umandap on 10/12/2018.
+//  Copyright © 2018 Jeffrey Umandap. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    public private(set) var message: String!
+    public private(set) var userName: String!
+    public private(set) var channelId: String!
+    public private(set) var userAvatar: String!
+    public private(set) var userAvatarColor: String!
+    public private(set) var id: String!
+    public private(set) var timeStamp: String!
+}
